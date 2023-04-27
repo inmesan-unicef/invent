3101bb610d4facf8ffd4234c7f99a489ccd43d34
ladeniva2 <122989825+ladeniva2@users.noreply.github.com>
2023-04-27 16:32:03 +0300

    technology platforms required false

--- frontend/utilities/projects.js
+++ frontend/utilities/projects.js
@@ -307,7 +307,7 @@ export const publishRules = () => {
     capability_categories: {},
     capability_subcategories: {},
     platforms: {
-      required: true,
+      required: false,
     },
     strategies: {
       required: true,
